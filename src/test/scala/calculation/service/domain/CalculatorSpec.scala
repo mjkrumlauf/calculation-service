f88b514ae9fd7314b4621cea63c3f9d8891e9af4
Mike Krumlauf <michael.krumlauf@lexisnexis.com>
2016-05-15 21:15:00 -0400

    Renaming and refinement

--- src/test/scala/calculation/service/domain/CalculatorSpec.scala
+++ src/test/scala/calculation/service/domain/CalculatorSpec.scala
@@ -4,47 +4,44 @@ import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestActors, TestKit}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-/**
-  * Created by mjkrumlauf on 5/15/16.
-  */
-class CalculatorActorSpec extends TestKit(ActorSystem("test")) with ImplicitSender
+class CalculatorSpec extends TestKit(ActorSystem("test")) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
 
-  val calculator = system.actorOf(Props(new CalculatorActor))
+  val calculator = system.actorOf(Props(new Calculator))
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
 
-  "A Calculator actor" must {
+  "A Calculator" must {
     "add two numbers and return the sum" in {
       val (add1, add2) = (2, 5)
-      this.calculator ! new Add(add1, add2)
-      expectMsg(new AddResult(add1, add2, 7))
+      this.calculator ! Add(add1, add2)
+      expectMsg(AddResult(add1, add2, 7))
     }
   }
 
   it must {
     "subtract two numbers and return the difference" in {
       val (sub1, sub2) = (10, 7)
-      this.calculator ! new Subtract(sub1, sub2)
-      expectMsg(new SubtractResult(sub1, sub2, 3))
+      this.calculator ! Subtract(sub1, sub2)
+      expectMsg(SubtractResult(sub1, sub2, 3))
     }
   }
 
   it must {
     "multiply two numbers and return the product" in {
       val (mult1, mult2) = (7, 4)
-      this.calculator ! new Multiply(mult1, mult2)
-      expectMsg(new MultiplicationResult(mult1, mult2, 28))
+      this.calculator ! Multiply(mult1, mult2)
+      expectMsg(MultiplicationResult(mult1, mult2, 28))
     }
   }
 
   it must {
     "divide two numbers and return the quotient" in {
       val (dividend, divisor) = (3, 2)
-      this.calculator ! new Divide(dividend, divisor)
-      expectMsg(new DivisionResult(dividend, divisor, 1.5))
+      this.calculator ! Divide(dividend, divisor)
+      expectMsg(DivisionResult(dividend, divisor, 1.5))
     }
   }
 }
