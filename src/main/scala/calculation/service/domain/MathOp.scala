5431ae3b881f8b7421a4b83bcb15665480ba7b12
Mike Krumlauf <michael.krumlauf@lexisnexis.com>
2016-07-11 21:43:40 -0400

    Modify MathOp to allow serialization of operator symbol

--- src/main/scala/calculation/service/domain/MathOp.scala
+++ src/main/scala/calculation/service/domain/MathOp.scala
@@ -1,18 +1,28 @@
 package calculation.service.domain
 
-trait MathOp
+abstract class MathOp(op: String)
 
-case class Add(nbr1: Int, nbr2: Int) extends MathOp
-case class Subtract(nbr1: Int, nbr2: Int) extends MathOp
-case class Multiply(nbr1: Int, nbr2: Int) extends MathOp
-case class Divide(nbr1: Double, nbr2: Int) extends MathOp
-case class Exponentiate(base: Double, exponent: Double) extends MathOp
+object ops {
+  val AddOp = "+"
+  val SubtractOp = "-"
+  val MultOp = "*"
+  val DivOp = "/"
+  val ExpOp = "^"
+}
 
-trait MathResult
+import ops._
 
-case class AddResult(nbr: Int, nbr2: Int, result: Int) extends MathResult
-case class SubtractResult(nbr1: Int, nbr2: Int, result: Int) extends MathResult
-case class MultiplicationResult(nbr1: Int, nbr2: Int, result: Int) extends MathResult
-case class DivisionResult(nbr1: Double, nbr2: Int, result: Double) extends MathResult
-case class ExponentiationResult(base: Double, exponent: Double, result: Double) extends MathResult
+case class Add(nbr1: Int, nbr2: Int) extends MathOp(AddOp)
+case class Subtract(nbr1: Int, nbr2: Int) extends MathOp(SubtractOp)
+case class Multiply(nbr1: Int, nbr2: Int) extends MathOp(MultOp)
+case class Divide(nbr1: Double, nbr2: Int) extends MathOp(DivOp)
+case class Exponentiate(base: Double, exponent: Double) extends MathOp(ExpOp)
+
+abstract class MathResult(op: String)
+
+case class AddResult(nbr: Int, nbr2: Int, result: Int) extends MathResult(AddOp)
+case class SubtractResult(nbr1: Int, nbr2: Int, result: Int) extends MathResult(SubtractOp)
+case class MultiplicationResult(nbr1: Int, nbr2: Int, result: Int) extends MathResult(MultOp)
+case class DivisionResult(nbr1: Double, nbr2: Int, result: Double) extends MathResult(DivOp)
+case class ExponentiationResult(base: Double, exponent: Double, result: Double) extends MathResult(ExpOp)
 
